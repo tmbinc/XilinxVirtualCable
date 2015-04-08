e7fe4fd61393d1bdee4a28504fd06feef5915d34
tclstoreuser <duthv@xilinx.com>
2015-04-08 17:31:57 -0600

    Created a new folder for the XAPP files

--- XAPP1251/xvcServer.c
+++ XAPP1251/xvcServer.c
@@ -59,6 +59,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <time.h>
+#include <stdint.h>
 
 #include <sys/mman.h>
 #include <fcntl.h>
@@ -66,65 +67,20 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/tcp.h>
-#include <netinet/in.h>
+#include <netinet/in.h> 
+#include <pthread.h>
 
 #define MAP_SIZE      0x10000
-#define LENGTH_OFFSET 0x00
-#define TMS_OFFSET    0x04
-#define TDI_OFFSET    0x08
-#define TDO_OFFSET    0x0C
-#define CTRL_OFFSET   0x10
 
-static int jtag_state;
-static int verbose;
-static volatile void *ptr;
-static int fd_uio;
-static int commandStage = 0;
+typedef struct {
+  uint32_t  length_offset;
+  uint32_t  tms_offset;
+  uint32_t  tdi_offset;
+  uint32_t  tdo_offset;
+  uint32_t  ctrl_offset;
+} jtag_t;
 
-enum {
-   test_logic_reset,
-   run_test_idle,
-
-   select_dr_scan,
-   capture_dr,
-   shift_dr,
-   exit1_dr,
-   pause_dr,
-   exit2_dr,
-   update_dr,
-
-   select_ir_scan,
-   capture_ir,
-   shift_ir,
-   exit1_ir,
-   pause_ir,
-   exit2_ir,
-   update_ir,
-
-   num_states
-};
-
-static int jtag_step(int state, int tms) {
-   static const int next_state[num_states][2] =
-   { [test_logic_reset] = { run_test_idle , test_logic_reset },
-     [run_test_idle]    = { run_test_idle , select_dr_scan   },
-     [select_dr_scan]   = { capture_dr    , select_ir_scan   },
-     [capture_dr]       = { shift_dr      , exit1_dr         },
-     [shift_dr]         = { shift_dr      , exit1_dr         },
-     [exit1_dr]         = { pause_dr      , update_dr        },
-     [pause_dr]         = { pause_dr      , exit2_dr         },
-     [exit2_dr]         = { shift_dr      , update_dr        },
-     [update_dr]        = { run_test_idle , select_dr_scan   },
-     [select_ir_scan]   = { capture_ir    , test_logic_reset },
-     [capture_ir]       = { shift_ir      , exit1_ir         },
-     [shift_ir]         = { shift_ir      , exit1_ir         },
-     [exit1_ir]         = { pause_ir      , update_ir        },
-     [pause_ir]         = { pause_ir      , exit2_ir         },
-     [exit2_ir]         = { shift_ir      , update_ir        },
-     [update_ir]        = { run_test_idle , select_dr_scan   } };
-
-   return next_state[state][tms];
-}
+static int verbose = 0;
 
 static int sread(int fd, void *target, int len) {
    unsigned char *t = target;
@@ -138,10 +94,9 @@ static int sread(int fd, void *target, int len) {
    return 1;
 }
 
-int handle_data(int fd) {
-	int i, n;
-	int seen_tlr = 0;
-	const char xvcInfo[] = "xvcServer_v1.0:2048\n";
+int handle_data(int fd, volatile jtag_t* ptr) {
+
+	const char xvcInfo[] = "xvcServer_v1.0:2048\n"; 
 
 	do {
 		char cmd[16];
@@ -180,9 +135,9 @@ int handle_data(int fd) {
 		} else if (memcmp(cmd, "sh", 2) == 0) {
 			if (sread(fd, cmd, 4) != 1)
 				return 1;
-//			if (verbose) {
+			if (verbose) {
 				printf("%u : Received command: 'shift'\n", (int)time(NULL));
-//			}
+			}
 		} else {
 
 			fprintf(stderr, "invalid cmd '%s'\n", cmd);
@@ -210,32 +165,13 @@ int handle_data(int fd) {
 		if (verbose) {
 			printf("\tNumber of Bits  : %d\n", len);
 			printf("\tNumber of Bytes : %d \n", nr_bytes);
-//			for (i = 0; i < nr_bytes * 2; ++i)
-//				printf("\t0x%02x \n", buffer[i]);
 			printf("\n");
 		}
-//
-		seen_tlr = (seen_tlr || jtag_state == test_logic_reset)
-				&& (jtag_state != capture_dr) && (jtag_state != capture_ir);
-
-		if ((jtag_state == exit1_ir && len == 5 && buffer[0] == 0x17)
-				|| (jtag_state == exit1_dr && len == 4 && buffer[0] == 0x0b)) {
-			if (verbose)
-				printf("ignoring bogus jtag state movement in jtag_state %d\n",
-						jtag_state);
-		} else {
-//
 
 			int bytesLeft = nr_bytes;
 			int bitsLeft = len;
 			int byteIndex = 0;
 			int tdi, tms, tdo;
-
-			volatile ptr = mmap(NULL, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED,
-					fd_uio, 0);
-
-			if (ptr == MAP_FAILED)
-				fprintf(stderr, "MMAP Failed\n");
 
 			while (bytesLeft > 0) {
 				tms = 0;
@@ -245,20 +181,21 @@ int handle_data(int fd) {
 					memcpy(&tms, &buffer[byteIndex], 4);
 					memcpy(&tdi, &buffer[byteIndex + nr_bytes], 4);
 
-					*((unsigned *) (ptr + LENGTH_OFFSET)) = 32;
-					*((unsigned *) (ptr + TMS_OFFSET)) = tms;
-					*((unsigned *) (ptr + TDI_OFFSET)) = tdi;
-					*((unsigned *) (ptr + CTRL_OFFSET)) = 0x01;
+					ptr->length_offset = 32;        
+					ptr->tms_offset = tms;         
+					ptr->tdi_offset = tdi;       
+					ptr->ctrl_offset = 0x01;
 
 					/* Switch this to interrupt in next revision */
-					while (*((unsigned *) (ptr + CTRL_OFFSET)))
-						;
+					while (ptr->ctrl_offset)
+						{
+            }
 
-					tdo = *((unsigned *) (ptr + TDO_OFFSET));
+					tdo = ptr->tdo_offset;
 					memcpy(&result[byteIndex], &tdo, 4);
 
 					bytesLeft -= 4;
-					bitsLeft -= 32;
+					bitsLeft -= 32;         
 					byteIndex += 4;
 
 					if (verbose) {
@@ -271,14 +208,18 @@ int handle_data(int fd) {
 				} else {
 					memcpy(&tms, &buffer[byteIndex], bytesLeft);
 					memcpy(&tdi, &buffer[byteIndex + nr_bytes], bytesLeft);
-					*((unsigned *) (ptr + LENGTH_OFFSET)) = bitsLeft;
-					*((unsigned *) (ptr + TMS_OFFSET)) = tms;
-					*((unsigned *) (ptr + TDI_OFFSET)) = tdi;
-					*((unsigned *) (ptr + CTRL_OFFSET)) = 0x01;
+          
+					ptr->length_offset = bitsLeft;        
+					ptr->tms_offset = tms;         
+					ptr->tdi_offset = tdi;       
+					ptr->ctrl_offset = 0x01;
 					/* Switch this to interrupt in next revision */
-					while (*((unsigned *) (ptr + CTRL_OFFSET)))
-						;
-					tdo = *((unsigned *) (ptr + TDO_OFFSET));
+					while (ptr->ctrl_offset)
+						{
+            }
+
+					tdo = ptr->tdo_offset;
+          
 					memcpy(&result[byteIndex], &tdo, bytesLeft);
 
 					if (verbose) {
@@ -287,19 +228,15 @@ int handle_data(int fd) {
 						printf("TDI : 0x%08x\n", tdi);
 						printf("TDO : 0x%08x\n", tdo);
 					}
-
 					break;
 				}
 			}
-			munmap(ptr, MAP_SIZE);
-		}
-
 		if (write(fd, result, nr_bytes) != nr_bytes) {
 			perror("write");
 			return 1;
 		}
 
-	} while (!(seen_tlr && jtag_state == run_test_idle));
+	} while (1);
 	/* Note: Need to fix JTAG state updates, until then no exit is allowed */
 	return 0;
 }
@@ -307,8 +244,12 @@ int handle_data(int fd) {
 int main(int argc, char **argv) {
    int i;
    int s;
-   int c;
+   int c; 
+   int fd_uio;
+   
    struct sockaddr_in address;
+   
+
 
    opterr = 0;
 
@@ -322,19 +263,26 @@ int main(int argc, char **argv) {
          return 1;
       }
 
-   fd_uio = open("/dev/uio0", O_RDWR | O_SYNC);
+   fd_uio = open("/dev/uio0", O_RDWR );
    if (fd_uio < 1) {
       fprintf(stderr,"Failed to Open UIO Device\n");
       return -1;
    }
 
    s = socket(AF_INET, SOCK_STREAM, 0);
-
+               
    if (s < 0) {
       perror("socket");
       return 1;
    }
 
+   volatile jtag_t* ptr = (volatile jtag_t*) mmap(NULL, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED,
+					fd_uio, 0);
+   if (ptr == MAP_FAILED)
+				fprintf(stderr, "MMAP Failed\n");   
+        
+   close(fd_uio);
+   
    i = 1;
    setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &i, sizeof i);
 
@@ -397,7 +345,7 @@ int main(int argc, char **argv) {
                   FD_SET(newfd, &conn);
                }
             }
-            else if (handle_data(fd)) {
+            else if (handle_data(fd,ptr)) {
 
                if (verbose)
                   printf("connection closed - fd %d\n", fd);
@@ -415,5 +363,6 @@ int main(int argc, char **argv) {
          }
       }
    }
+   munmap((void *) ptr, MAP_SIZE);
    return 0;
 }
